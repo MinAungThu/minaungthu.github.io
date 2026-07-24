0c30d2f9e90052cf801ea94c279b5b834a6923ef
MinAungThu <88431515+MinAungThu@users.noreply.github.com>
2026-07-24 16:17:44 +0000

    Deploying to gh-pages from @ MinAungThu/minaungthu.github.io@685d509f046cff272a904d115d153a998731d4de 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,9 +9,9 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-projects",
-          title: "projects",
-          description: "A growing collection of your cool projects.",
+  },{id: "nav-personal-projects",
+          title: "Personal Projects",
+          description: "Personally, I like studying fundamentals and solidifying my foundations to build strong intuitive understanding.  This is a collection of custom integrated circuit designs focusing on fundamentals and first principles.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/projects/";
